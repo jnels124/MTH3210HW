6a64597d2bb16d2d73c46d652551a0abd94c96bc
Jesse Nelson <jnels124@msudenver.edu>
2013-08-25 01:09:38 -0600

    Added quick sort to help with 5 number summary. Still needs to be tested

--- HW1/QuickSort.java
+++ HW1/QuickSort.java
@@ -1,4 +1,33 @@
+import java.io.*;
+import java.util.*;
 
 public class QuickSort {
+    public static void swap ( int population[], int thisInt, int  thatInt ) {
+        int temp = population[thisInt];
+        population[thisInt] = population[thatInt];
+        population[thatInt] = temp;
+    }
 
+    // Reorganizes the given list so all elements less than the first are 
+    // before it and all greater elements are after it.                   
+    public static int partition( int population[], int first, int last ) {
+        int pivot = population[first];
+        while ( first < last ) {
+            if ( population[first] == pivot || population[last] == pivot ) {
+                
+            }
+            while ( population[first] < pivot ) first++;
+            while ( population[last] > pivot ) last--;
+            swap ( population, first, last );
+        }
+        return first;
+    }
+
+    public static void Quicksort( int population[], int first, int last ) {
+        if ( first >= last ) return;
+        int pivot_index = partition( population, first, last );
+        Quicksort( population, first, pivot_index );
+        Quicksort( population, pivot_index+1, last );
+    }
+    
 }
